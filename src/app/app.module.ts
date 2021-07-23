6316168f8e52abbab6cd9682984cf00f6aacc02c
Yehya <yehyajr97@gmail.com>
2021-07-24 00:08:52 +0200

    fix

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,7 +3,7 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { ProfileComponent } from '../profile/profile.component';
+import { ProfileComponent } from './profile/profile.component';
 import { FormsModule } from '@angular/forms';
 import { RegistrationComponent } from './registration/registration.component';
 import { FormComponent } from './registration/form/form.component';
