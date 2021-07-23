9c1f454619ae5981d85695051daa3a4b09e9ff37
Ahmed Magdy <79047391+ahmedelsangek@users.noreply.github.com>
2021-07-23 13:50:04 +0200

    Create Form

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,31 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ProfileComponent } from '../profile/profile.component';
+import { FormsModule } from '@angular/forms';
+import { RegistrationComponent } from './registration/registration.component';
+import { FormComponent } from './registration/form/form.component';
+import { EmailComponent } from './registration/form/email/email.component';
+import { NameComponent } from './registration/form/name/name.component';
+import { DateComponent } from './registration/form/date/date.component';
+import { GenderComponent } from './registration/form/gender/gender.component';
+import { PasswordComponent } from './registration/form/password/password.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ProfileComponent
+    ProfileComponent,
+    RegistrationComponent,
+    FormComponent,
+    EmailComponent,
+    NameComponent,
+    DateComponent,
+    GenderComponent,
+    PasswordComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
