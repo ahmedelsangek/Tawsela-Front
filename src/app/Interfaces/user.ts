4f392d1f3487795bc5d514a2afdfcc4e141c9b19
Mark-abdallah <83455664+Mark-abdallah@users.noreply.github.com>
2021-07-24 21:07:32 +0200

    asd

---
+++ src/app/Interfaces/user.ts
@@ -0,0 +1,8 @@
+export class User {
+    email?: string;
+    firstName?: string;
+    lastName?:string;
+    gender?: string;
+    password?: string;
+    birthday?: string;
+}
--- src/app/registration/form/form.component.ts
+++ src/app/registration/form/form.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { User } from 'src/app/Interfaces/user';
 
 @Component({
   selector: 'app-form',
@@ -6,10 +7,31 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./form.component.scss']
 })
 export class FormComponent implements OnInit {
+  email=true;
+  name=false;
+  date=false;
+  gender=false;
+  password=false;
 
   constructor() { }
-
+  appUser= new User();
   ngOnInit(): void {
   }
-
+  next(){
+    if (this.email==true) {
+      this.email=false;
+      this.name=true;
+    }else if(this.name==true){
+      this.name=false;
+      this.date=true;
+    }else if(this.date==true){
+      this.date=false;
+      this. gender=true;
+    }else if(this.gender==true){
+      this. gender=false;
+      this. password=true;
+  }else if(this.password==true){
+    this. password=false;
+}
+  }
 }
