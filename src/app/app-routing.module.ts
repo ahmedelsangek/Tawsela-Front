75717c3906444db7c67785817f384740a51e39b3
Mark-abdallah <83455664+Mark-abdallah@users.noreply.github.com>
2021-07-25 19:13:40 +0200

    asd

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { MainPageComponent } from './main-page/main-page.component';
 import { OfferSeatsComponent } from './offer-seats/offer-seats.component';
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 import { ProfileComponent } from './profile/profile.component';
@@ -7,7 +8,8 @@ import { FormComponent } from './registration/form/form.component';
 import { RegistrationComponent } from './registration/registration.component';
 
 const routes: Routes = [
-  {path:"",redirectTo:"/home",pathMatch:"full"},
+ // {path:"",redirectTo:"home",pathMatch:"full"},
+  {path:"home",component:MainPageComponent},
   {path:"offer-seats",component:OfferSeatsComponent},
   {path:"profile",component:ProfileComponent},
   {path:"sign-up",component:RegistrationComponent},
